fc61d9f3f0537e0bdc5a75244b77e45b841f637a
Hachounet <hachounet.github@gmx.fr>
2023-11-30 16:57:40 +0100

    working on exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+  string = "hey";
+for ( num = 0; num < 10; num++ ) {
+    if ( num === 3 ) {
+        continue;
+    }
+    else if ( num === 10) {
+        continue;
+    }
+    
+}
+return string.repeat(num)
+} 
+
 
 // Do not edit below this line
 module.exports = repeatString;
