8a4d7c02c507d3f19a0041d25d6d7188c32f5556
Hachounet <hachounet.github@gmx.fr>
2023-12-04 11:33:54 +0100

    Finishing ex4 ex 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -4,12 +4,19 @@ const removeFromArray = function(myArray, ...toRemove) {
     let result = [];
 
     for ( let i = 0; i < myArray.length; i++ ) {
-        if ( myArray[i] !== newArray[i] ) {
-            result.push(myArray[i])
+       let match = false;
+       for (let j = 0; j < newArray.length; j++) {
+        if (myArray[i] === newArray[j] ) {
+            match = true;
+            break;
+
         }
+       }
+       if (!match) {
+        result.push(myArray[i]);
+       }
     }
     return result;
-
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
-};
+    let totalSum = b;
+    if (typeof a === 'number' && a >= 0 && typeof b === 'number' && b >= 0) {
+    if (a < b ) {
+        for ( let i = a; i < b; i++) {
+            totalSum += i
+        }
+
+    }
+    else if ( a > b ) {
+        for ( let i = a; i > b; i--)
+        totalSum += i;
+    }
+    
+
+return totalSum
+}
+else { 
+    return totalSum = 'ERROR';
+}
+}
 
 // Do not edit below this line
 module.exports = sumAll;
