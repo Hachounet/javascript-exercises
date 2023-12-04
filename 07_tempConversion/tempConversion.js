2668d9247594f8a4e9045e3f542320959d36f435
Hachounet <hachounet.github@gmx.fr>
2023-12-04 14:21:42 +0100

    finishing ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,30 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+
+  let inFah
+  let inCelsius
+
+  (inCelsius = (a -32) * (5/9) )
+  inCelsius = inCelsius.toFixed(1);
+  return parseFloat(inCelsius) 
+
+
+
+
+
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(b) {
+
+  let inFah
+
+  inFah = (b * (9/5) + 32 ) 
+  inFah = inFah.toFixed(1);
+  return parseFloat(inFah)
+
+
+
 };
 
 // Do not edit below this line
