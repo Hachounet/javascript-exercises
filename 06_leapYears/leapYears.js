0f0b276574dc24ab27b22c4f00d6998b8c11f75d
Hachounet <hachounet.github@gmx.fr>
2023-12-04 13:41:41 +0100

    finishing ex6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let result = false;
+    if ( year % 4 === 0 && year % 100 !== 0) {
+        return result = true;
+        
+    }
+        else if ( year % 100 === 0 && year % 400 === 0){
+            return result = true;
+        }
+
+    else {
+        return result = false;
+    }
+console.log(typeof(result));
 };
 
 // Do not edit below this line
