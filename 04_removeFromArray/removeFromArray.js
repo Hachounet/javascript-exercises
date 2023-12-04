e85a86b7981feb1cec269d606517d8bdf7799f61
Hachounet <hachounet.github@gmx.fr>
2023-12-04 10:00:32 +0100

    working on ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,16 @@
-const removeFromArray = function(myArray, toRemove) {
+const removeFromArray = function(myArray, ...toRemove) {
 
-    let myArray = "";
-    if 
+    let newArray = [...toRemove];
+    let result = [];
+
+    for ( let i = 0; i < myArray.length; i++ ) {
+        if ( myArray[i] !== newArray[i] ) {
+            result.push(myArray[i])
+        }
+    }
+    return result;
 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
-f
