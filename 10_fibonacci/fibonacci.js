f0e5ddd9c38966d141986115615640d0fdf3afff
Hachounet <hachounet.github@gmx.fr>
2023-12-15 16:33:57 +0100

    ex 11 DONE

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -6,15 +6,12 @@ const fibonacci = function(input) {
     if ( input < 0){
         return input = "OOPS";
     }
+    else if ( input === 1 || input === 2){
+        return result = 1;
+    }
     else {
         let fibonarray = [1, 1]
         console.log(`test, this is my fibonarray before forloop : ${fibonarray}`)
-        if ( input === 1){
-            return result = 1;}
-        else if (input === 2){
-            return result = 1;
-        }
-
             for ( let i = 2; i < input; i++ ){
                 
                 fibonarray.push(fibonarray[i-1] + fibonarray[i-2])
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
-};
+
+
+  function getTheTitles(input) {
+    return input.reduce((acc, currentValue) => {
+        acc.push(currentValue.title);
+        return acc;
+    }, []);
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
