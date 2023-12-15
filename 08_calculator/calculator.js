a6a92e5f9a5e871d510e242b8365457404e332be
Hachounet <hachounet.github@gmx.fr>
2023-12-15 09:24:13 +0100

    ex 8 DONE

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+  let sum = ( Number(a)+ Number(b));
+	return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b){
+  let substract = Number(a) - Number(b);
+  return substract;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let summing = array.reduce((acc, currentValue) => acc + currentValue, 0)
+	return summing;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiply = array.reduce((acc, currentValue) => acc * currentValue)
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let power = Number(a) ** Number(b)
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  return n > 1?n * factorial(n-1):1;
 };
 
 // Do not edit below this line
