b2594f8753dbfe76a62b6976a0157170cd35945f
Hachounet <hachounet.github@gmx.fr>
2023-12-15 11:24:45 +0100

    ex 9 DONE

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let regex = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(regex, "")
+    let len = str.length;
+
+    for ( let i = 0; i < len /2; i++){
+        if (str[i] !== str[len - 1 - i]){
+            return false;
+        }
+    }
+    return true;
+    
+    /*
+    let cleanedStringToArray = cleanedString.split()
+    let reversedArray = cleanedStringToArray.reverse()
+    let rejoinArray = reversedArray.join()
+    console.log(`rejoinArray = ${rejoinArray}`)
+    console.log(`reversedArray = ${reversedArray}`)
+    if ( rejoinArray === cleanedString){
+        return true;
+    }
+    else {
+        return false;
+    } */
 };
 
 // Do not edit below this line
